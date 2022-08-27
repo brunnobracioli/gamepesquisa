b6ea4639f251d5b54b9a9d95435d796d57cd1b46
brunnobracioli <brunno_012@hotmail.com>
2022-08-27 10:31:54 -0300

    criação pasta backend

---
+++ backend/src/test/java/com/gamepesquisa/GamepesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamepesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
